027cf1053066477b0de5994d226e54ff5623bf58
Voytek Pituła <w.pitula@gmail.com>
2026-05-06 07:17:33 +0200

    Add dmn viewer (#100)
    * Add dmn viewer
    
    * scalafmt
    
    * review fixes
    
    * fix tests

--- decisions4s-dmn/src/main/scala/decisions4s/dmn/DMNModel.scala
+++ decisions4s-dmn/src/main/scala/decisions4s/dmn/DMNModel.scala
@@ -2,6 +2,34 @@ package decisions4s.dmn
 
 import org.camunda.bpm.model.dmn.{Dmn, DmnModelInstance}
 
+import java.io.ByteArrayOutputStream
+import java.nio.charset.StandardCharsets
+import java.util.Base64
+import java.util.zip.{Deflater, DeflaterOutputStream}
+
 case class DMNModel(raw: DmnModelInstance) {
   def toXML: String = Dmn.convertToString(raw)
+
+  def shareUrl(viewerUrl: String = DMNModel.DefaultViewerUrl): String = {
+    val deflated = DMNModel.deflate(toXML.getBytes(StandardCharsets.UTF_8))
+    val encoded  = Base64.getUrlEncoder.withoutPadding.encodeToString(deflated)
+    s"$viewerUrl${DMNModel.HashPrefix}$encoded"
+  }
+}
+
+object DMNModel {
+
+  val DefaultViewerUrl = "https://business4s.github.io/decisions4s/dmn-viewer/"
+  val HashPrefix       = "#dmn="
+
+  private def deflate(input: Array[Byte]): Array[Byte] = {
+    val out      = new ByteArrayOutputStream()
+    val deflater = new Deflater(Deflater.BEST_COMPRESSION)
+    try {
+      val dos = new DeflaterOutputStream(out, deflater)
+      try dos.write(input)
+      finally dos.close()
+    } finally deflater.end()
+    out.toByteArray
+  }
 }
